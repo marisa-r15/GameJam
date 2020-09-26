369b80ba1c59b30fa3555364b26e9b5955abce89
marisa-r15 <marisa.ramey15@gmail.com>
2020-09-25 18:29:30 -0700

    Player and Enemy animations and movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public bool MoveRight;
+    private Animator EnemyAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoveRight){
+            transform.Translate(2 * Time.deltaTime * speed , 0 , 0);
+            EnemyAnim.SetBool("MoveRight",true);
+
+        }
+        else{
+            transform.Translate(-2 * Time.deltaTime * speed , 0 , 0);
+            EnemyAnim.SetBool("MoveRight",false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D PlayerBody;
+    private Vector3 change;
+    private Animator PlayerAnim;
+
+    void Start()
+    {
+        PlayerBody = GetComponent<Rigidbody2D>();
+        PlayerAnim = GetComponent<Animator>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        PlayerAnimAndMove();
+        MoveCharacter();
+    }
+
+    void PlayerAnimAndMove()
+    {
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            PlayerAnim.SetFloat("MoveX", change.x);
+            PlayerAnim.SetFloat("MoveY", change.y);
+            PlayerAnim.SetBool("Moving", true);
+            }else{PlayerAnim.SetBool("Moving",false);}
+    }
+    void MoveCharacter()
+    {
+        PlayerBody.MovePosition(
+        transform.position + change * speed * Time.deltaTime);
+    }
+}
